466295ecec5d43b5091adcdf7c5aa378fae5eea4
Kaarti22 <kaartimondi@gmail.com>
2025-02-10 13:48:16 +0530

    adding new users to doctors list

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -2,14 +2,23 @@
 
 import React, { useState } from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { auth } from "@/app/firebase/config";
+import { auth, db } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
+import { doc, setDoc } from "firebase/firestore";
+import { updateProfile } from "firebase/auth";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-  const [createUserWithEmailAndPassword] =
+  const [createUserWithEmailAndPassword, userCredential, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
 
   const router = useRouter();
@@ -19,13 +28,35 @@ const SignUp = () => {
       const response = await createUserWithEmailAndPassword(email, password);
       if (!response?.user) {
         console.log("Email or password already exists. Check and try again.");
-      } else {
-        const userId = response.user.uid;
-        sessionStorage.setItem("user", userId);
-        setEmail("");
-        setPassword("");
-        router.push(`/dashboard/${userId}`);
+        return;
       }
+
+      const userId = response.user.uid;
+      const displayName = `${firstName} ${lastName}`;
+
+      await updateProfile(response.user, { displayName });
+
+      await setDoc(doc(db, "doctors", userId), {
+        id: userId,
+        profileImage: "",
+        firstName,
+        lastName,
+        displayName,
+        designation,
+        phoneNumber,
+        emailAddress: email,
+      });
+
+      sessionStorage.setItem("user", userId);
+
+      setEmail("");
+      setPassword("");
+      setFirstName("");
+      setLastName("");
+      setDesignation("");
+      setPhoneNumber("");
+
+      router.push(`/dashboard/${userId}`);
     } catch (error) {
       console.error("Error creating user", error);
     }
@@ -33,28 +64,52 @@ const SignUp = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96 flex flex-col gap-4 items-center">
         <h1 className="text-white text-2xl mb-5">Sign Up</h1>
-        <input
+
+        <Input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Designation"
+          value={designation}
+          onChange={(e) => setDesignation(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <Separator />
+        <Input
           type="email"
-          placeholder="Enter your email"
+          placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
-        <input
+        <Input
           type="password"
-          placeholder="Enter a password"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
-        <button
-          onClick={handleSignUp}
-          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
-        >
-          Sign Up
-        </button>
+        <Button onClick={handleSignUp} disabled={loading} className="w-full">
+          {loading ? "Signing up..." : "Sign up"}
+        </Button>
+        {error && <p className="text-red-500 mt-2">{error.message}</p>}
+        <Separator/>
+        <p className="text-sm text-white">Already have an account? <Button size={"sm"} variant={"link"} onClick={() => router.push(`/sign-in`)}>Sign in</Button></p>
       </div>
     </div>
   );
