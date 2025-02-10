aee88dad848936b2796f3b947b2c7fbff13b2ace
Kaarti22 <kaartimondi@gmail.com>
2025-02-10 12:57:42 +0530

    profile redirect and logout functionality

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+import { getAuth, signOut } from "firebase/auth";
 import { BellRing } from "lucide-react";
 import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
 import React from "react";
 
 interface HeaderProps {
@@ -7,6 +18,21 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
+  const auth = getAuth();
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      sessionStorage.removeItem("user");
+      router.push(`/sign-in`);
+    } catch (err) {
+      console.error("Error signing out: ", err);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between">
       <a href="/">
@@ -15,13 +41,29 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
       {title && <h2 className="text-2xl font-bold">{title}</h2>}
       <div className="flex items-center gap-5">
         <BellRing className="cursor-pointer" />
-        <Image
-          src={"/profileIcon.png"}
-          alt="Profile Icon"
-          height={30}
-          width={30}
-          className="rounded-full cursor-pointer shadow-md"
-        />
+        <Popover>
+          <PopoverTrigger>
+            <Image
+              src={"/profileIcon.png"}
+              alt="Profile Icon"
+              height={30}
+              width={30}
+              className="rounded-full cursor-pointer shadow-md"
+            />
+          </PopoverTrigger>
+          <PopoverContent className="w-auto flex flex-col justify-center gap-2">
+            <Button
+              variant={"ghost"}
+              onClick={() => router.push(`/dashboard/${id}/profile`)}
+            >
+              Profile settings
+            </Button>
+            <Separator />
+            <Button variant={"destructive"} onClick={handleLogout}>
+              Logout
+            </Button>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
