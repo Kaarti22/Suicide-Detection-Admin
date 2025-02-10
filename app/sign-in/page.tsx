31422d50965413cf583a6a72a2628470f1f69ca4
Kaarti22 <kaartimondi@gmail.com>
2025-02-10 14:42:01 +0530

    forgot password functionality

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -7,11 +7,13 @@ import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { sendPasswordResetEmail } from "firebase/auth";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [resetEmailSent, setResetEmailSent] = useState(false);
 
   const [signInWithEmailAndPassword, userCredential, loading, error] =
     useSignInWithEmailAndPassword(auth);
@@ -22,12 +24,11 @@ const SignIn = () => {
     try {
       const response = await signInWithEmailAndPassword(email, password);
       if (!response?.user) {
-        console.log("Email or password is incorrect. Check and try again.");
+        setMessage("Email or password is incorrect. Check and try again.");
         return;
       }
 
       if (!response.user.emailVerified) {
-        console.log("Please verify your email before signing in.");
         setMessage("Please verify your email before signing in.");
         return;
       }
@@ -39,7 +40,24 @@ const SignIn = () => {
 
       router.push(`/dashboard/${userId}`);
     } catch (error) {
-      console.error("Error signing in user", error);
+      console.error("Error signing in user: ", error);
+      setMessage("Error signing in. Please try again.");
+    }
+  };
+
+  const handleForgotPassword = async () => {
+    if (!email) {
+      setMessage("Please enter your email to reset the password.");
+      return;
+    }
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setResetEmailSent(true);
+      setMessage("Password reset email sent! Check your inbox.");
+    } catch (error) {
+      console.error("Error sending password reset email", error);
+      setMessage("Failed to send password reset email. Please try again.");
     }
   };
 
@@ -62,7 +80,17 @@ const SignIn = () => {
         <Button onClick={handleSignIn} disabled={loading} className="w-full">
           {loading ? "Signing in..." : "Sign in"}
         </Button>
-        {error && <p className="text-red-500 mt-2">{error.message}</p>}
+        {message && <p className="text-red-500 text-center mt-2">{message}</p>}
+        <Button variant={"link"} size={"sm"} onClick={handleForgotPassword}>
+          Forgot Password?
+        </Button>
+
+        {resetEmailSent && (
+          <p className="text-green-500 mt-2 text-sm text-center">
+            Check your email for reset instructions.
+          </p>
+        )}
+
         <Separator />
         <p className="text-sm text-white">
           Don't have an account?{" "}
