80d67dab82655b2ebb2cfc4e35efb13171136b14
Kaarti22 <kaartimondi@gmail.com>
2025-05-02 16:24:18 +0530

    sentiment analysis checking through fastapi server

--- app/api/receive-post/route.ts
+++ app/api/receive-post/route.ts
@@ -1,10 +1,7 @@
+import FormData from "form-data";
 import { NextRequest, NextResponse } from "next/server";
 import { adminDb } from "@/lib/firebaseAdmin";
-
-function runSentimentAnalysis(text: string): number {
-  const score = Math.min(1, text.length / 300);
-  return parseFloat(score.toFixed(2));
-}
+import axios from "axios";
 
 export async function POST(req: NextRequest) {
   const apiKey = req.headers.get("authorization");
@@ -33,17 +30,46 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  const sentimentScore = runSentimentAnalysis(content);
+  let finalSentiment = "NEUTRAL";
+
+  try {
+    const formData = new FormData();
+    formData.append("text", content);
+
+    const imageResponse = await fetch(imageUrl);
+    const imageBuffer = await imageResponse.arrayBuffer();
+    const buffer = Buffer.from(imageBuffer);
+
+    formData.append("image", buffer, {
+      filename: "uploaded_image.jpg",
+      contentType: "image/jpeg",
+      knownLength: buffer.length,
+    });
+
+    const sentimentResponse = await axios.post(
+      `${process.env.FASTAPI_SERVER_URL}/analyze/`,
+      formData,
+      {
+        headers: formData.getHeaders(),
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity,
+      }
+    );
+
+    finalSentiment = sentimentResponse.data.final_sentiment;
+  } catch (error: any) {
+    console.error("Error analyzing sentiment: ", error.message);
+  }
 
   await adminDb.collection("postSentiments").add({
     userHandle,
     postId,
     content,
-    sentimentScore,
+    sentiment: finalSentiment,
     imageUrl,
     timestamp: new Date(timestamp),
     createdAt: new Date(),
   });
 
-  return NextResponse.json({ message: "Post processed", sentimentScore });
+  return NextResponse.json({ message: "Post processed", finalSentiment });
 }
